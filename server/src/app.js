2708b80cc2515f2f9f6827d75861c0ed95d1cd2f
Xiaoyan-L <running923@gmail.com>
2017-10-15 01:08:26 -0700

    complete user register

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+const {sequelize} = require('./models')
+const config = require('./config/config')
+
 
 const app = express();
 
@@ -10,12 +13,10 @@ app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post('/register', (req, res) => {
-    res.send({
-        message: `Hello ${req.body.email} Your user was registered! have fun!`
-    })
-})
+require('./routes')(app)
 
-var listener = app.listen(process.env.PORT || 8081, function(){
-    console.log("your app is running at port " + listener.address().port);
-})
+sequelize.sync()
+    .then(() => {
+        app.listen(config.port)
+        console.log(`your app is running at port ${config.port}`)
+        })
---
+++ server/src/config/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    port: process.env.PORT || 8081,
+    db: {
+        database: process.env.DB_NAME || 'sshdb',
+        user: process.env.DB_USER || 'root',
+        password: process.env.DB_PASS || 'Running_123',
+        options: {
+            dialect: process.env.DIALECT || 'mysql',
+            host: process.env.HOST || 'localhost',
+            pool: {
+                max: 5,
+                min: 0,
+                idle: 10000
+            },
+            storage: './tabtracker.sqlite'
+        }
+    }
+}
